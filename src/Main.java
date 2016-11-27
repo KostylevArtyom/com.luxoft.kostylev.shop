08be664d07cb6a16a40db52244e7ee7aadaaf1c9
TemaCech <TemaCech@gmail.com>
2016-11-27 11:32:22 +0600

    MainWindow view added. Main class added.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import views.MainWindow;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame mainWindow = new MainWindow();
+        mainWindow.setVisible(true);
+    }
+}
---
+++ src/views/MainWindow.java
@@ -0,0 +1,11 @@
+package views;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow(){
+        super("Shop");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(1000, 800);
+    }
+}
