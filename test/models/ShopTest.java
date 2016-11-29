ad0230a31a0e6e56a730bf3396c1bcdca5aa963a
TemaCech <TemaCech@gmail.com>
2016-11-30 01:27:12 +0600

    Merged with feauture_database_refactoring branch.

--- test/models/ShopTest.java
+++ test/models/ShopTest.java
@@ -1,5 +1,6 @@
 package models;
 
+import exceptions.DatabaseClassIndexAlreadyExistException;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,16 +12,20 @@ public class ShopTest {
     @Before
     public void initialize() {
         shop = new Shop();
-        shop.addCustomer(new Customer("Alphonse"));
-        shop.addCustomer(new Customer("Barney"));
-        shop.addCustomer(new Customer("Clare"));
-        shop.addCustomer(new Customer("Demetra"));
-        shop.addCustomer(new Customer("Emma"));
+        try {
+            shop.addCustomer(new Customer("Alphonse"));
+            shop.addCustomer(new Customer("Barney"));
+            shop.addCustomer(new Customer("Clare"));
+            shop.addCustomer(new Customer("Demetra"));
+            shop.addCustomer(new Customer("Emma"));
 
-        shop.addStock(new Stock(new Good("Flask"), 2, 110.0));
-        shop.addStock(new Stock(new Good("Tango"), 3, 125.0));
-        shop.addStock(new Stock(new Good("Clarity"), 3, 50.0));
-        shop.addStock(new Stock(new Good("Mango"), 2, 150.0));
+            shop.addStock(new Stock(new Good("Flask"), 2, 110.0));
+            shop.addStock(new Stock(new Good("Tango"), 3, 125.0));
+            shop.addStock(new Stock(new Good("Clarity"), 3, 50.0));
+            shop.addStock(new Stock(new Good("Mango"), 2, 150.0));
+        } catch (DatabaseClassIndexAlreadyExistException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
