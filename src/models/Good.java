75d347ce76126ecbdcd5ff9fcb9c46b64521a654
TemaCech <TemaCech@gmail.com>
2016-11-29 08:24:08 +0600

    Good toString method added.

--- src/models/Good.java
+++ src/models/Good.java
@@ -1,5 +1,10 @@
 package models;
 
+import models.utils.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Good {
     private static Integer id_counter = 0;
 
@@ -16,6 +21,20 @@ public class Good {
             id_counter = id + 1;
         this.id = id;
         this.description = description;
+    }
+
+    private class StorablePositions {
+        static final int ID = 1;
+        static final int DESCRIPTION = 2;
+    }
+
+    @Override
+    public String toString() {
+        List<String> store = new ArrayList<>();
+        store.add(getClass().getSimpleName());
+        store.add(StorablePositions.ID, getId().toString());
+        store.add(StorablePositions.DESCRIPTION, getDescription());
+        return String.join(Constants.STORE_SEPARATOR, store);
     }
 
     public Integer getId() {
